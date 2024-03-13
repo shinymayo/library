584dc5c1bb7ae297f7409cb83a04f761832267f0
shinymayo <138251809+shinymayo@users.noreply.github.com>
2024-03-13 14:10:36 +0000

    Make functions to add books and loop through arr
    Made addBookToLibrary function to add new book to
    the library array. And displayBooks function to
    loop through books, adding them to the card and
    displaying them on the page.
    
    Minor bug - even though classes were added to the
    book only some of the styles are applied.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const libraryDisplay = document.querySelector(".main");
 // book constructor
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -10,8 +11,9 @@ function Book(title, author, pages, isRead) {
 const theHobbit = new Book("The Hobbit", "J.R.R.Tolkien", "not read");
 const deathlyHallows = new Book("Harry Potter and the Deathly Hallows", "J.K.Rowling", "read");
 
+const darkTower = new Book("The Dark Tower", "Stephen King", 1000, "read");
 // create library array, with default books inside
-const library = [deathlyHallows, theHobbit];
+const library = [deathlyHallows];
 
 // function to add books
 function addBooktoLibrary(item) {
@@ -21,6 +23,43 @@ function addBooktoLibrary(item) {
 // function to display books on the page
 function displayBooks(array) {
     for (let item of array) {
-      console.log(item.title);
+        // check its in the array
+        console.log(item.title);
+        // create card
+        const bookCard = document.createElement("div");
+        bookCard.classList.add(".card");
+        // create parts of the card
+        const bookTitle = document.createElement("p");
+        bookTitle.classList.add(".book-title");
+        bookTitle.textContent = item.title;
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.classList.add(".book-author");
+        bookAuthor.textContent = item.author;
+
+        const bookPagesNum = document.createElement("p");
+        bookPagesNum.classList.add(".book-pages");
+        bookPagesNum.textContent = item.pages;
+
+        const removeButton = document.createElement("button");
+        removeButton.setAttribute("id", "remove-btn");
+        removeButton.textContent = "REMOVE";
+
+        const statusButton = document.createElement("button");
+        statusButton.setAttribute("id", "status-btn");
+        statusButton.textContent = "I HAVE READ IT";
+        //append to the card
+        bookCard.append(bookTitle, bookAuthor, bookPagesNum, removeButton, statusButton);
+        //append card to the library
+        libraryDisplay.append(bookCard);
+
     }
 }
+addBooktoLibrary(theHobbit);
+addBooktoLibrary(darkTower);
+displayBooks(library);
+
+// create function for remove button
+
+
+// create function for changing status button
