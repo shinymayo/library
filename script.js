bc2f6ad165288e604a1cae81f0efc1e78413802e
shinymayo <138251809+shinymayo@users.noreply.github.com>
2024-03-13 14:43:46 +0000

    Fix styling bug
    Fixed the styles bug- not all classes were applied
    to the card and it's elements. The issue was with
    the classList names - a dott was before them and
    needed to be removed

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
+    // this.isRead = isRead;
 }
 
 // create default books
-const theHobbit = new Book("The Hobbit", "J.R.R.Tolkien", "not read");
-const deathlyHallows = new Book("Harry Potter and the Deathly Hallows", "J.K.Rowling", "read");
+const theHobbit = new Book("The Hobbit", "J.R.R.Tolkien", 310);
+const deathlyHallows = new Book("Harry Potter and the Deathly Hallows", "J.K.Rowling", 784);
 
-const darkTower = new Book("The Dark Tower", "Stephen King", 1000, "read");
+const darkTower = new Book("The Dark Tower", "Stephen King", 224);
 // create library array, with default books inside
 const library = [deathlyHallows];
 
@@ -27,18 +27,18 @@ function displayBooks(array) {
         console.log(item.title);
         // create card
         const bookCard = document.createElement("div");
-        bookCard.classList.add(".card");
+        bookCard.classList.add("card");
         // create parts of the card
         const bookTitle = document.createElement("p");
-        bookTitle.classList.add(".book-title");
+        bookTitle.classList.add("book-title");
         bookTitle.textContent = item.title;
 
         const bookAuthor = document.createElement("p");
-        bookAuthor.classList.add(".book-author");
+        bookAuthor.classList.add("book-author");
         bookAuthor.textContent = item.author;
 
         const bookPagesNum = document.createElement("p");
-        bookPagesNum.classList.add(".book-pages");
+        bookPagesNum.classList.add("book-pages");
         bookPagesNum.textContent = item.pages;
 
         const removeButton = document.createElement("button");
